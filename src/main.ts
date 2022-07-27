3b4196b0b4fba26709869de95414093ebc9682ad
RawanHesham <97854538+RawanHesham20@users.noreply.github.com>
2022-07-27 17:26:13 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('v1');
   app.enableCors();
-  // app.use(morgan('dev'));
+  app.use(morgan('dev'));
   // app.useGlobalPipes(new ValidationPipe({transform:true}));
 
   console.log('hello');
